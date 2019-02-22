f7bb2415a16d04cfd802b9726016c34f8b64c880
Aristeidis Panagiotopoulos <arispanagiwtopoulos@gmail.com>
2019-02-23 01:19:10 +0200

    seamlessly hide and show toolbar

--- NoteTracker/UseCases/noteTableUseCase/MainViewController.swift
+++ NoteTracker/UseCases/noteTableUseCase/MainViewController.swift
@@ -7,15 +7,47 @@ protocol ReloadDelegate {
 protocol CollapseCreateDelegate {
     func collapseCreateViewController()
 }
+extension MainViewController: UISearchResultsUpdating {
 
+    func updateSearchResults(for searchController: UISearchController) {
+
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+    
+    // MARK: - Private instance methods
+    
+    func searchBarIsEmpty() -> Bool {
+        // Returns true if the text is empty or nil
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        
+        if let data = tableViewController?.data {
+            filteredNotes = data.filter({( note : NoteModel) -> Bool in
+                return note.text.lowercased().contains(searchText.lowercased()) ||
+                note.description.lowercased().contains(searchText.lowercased())
+            })
+        }
+        
+        tableViewController?.tableView.reloadData()
+    }
+    
+    func isFiltering() -> Bool {
+        return searchController.isActive && !searchBarIsEmpty()
+    }
+}
 class MainViewController: KeyboardHandler, ReloadDelegate, AdjustHeightDelegate, CollapseCreateDelegate {
         
     @IBOutlet weak var noteTableView: UIView!
     @IBOutlet weak var textContainerView: UIView!
-    
+
     @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var createContainerHeight: NSLayoutConstraint!
     
+    let searchController = UISearchController(searchResultsController: nil)
+    var filteredNotes = [NoteModel]()
+
     @IBAction func onDeletePressed(_ sender: Any) {
         tableViewController?.onDeletePressed()
     }
@@ -25,8 +57,10 @@ class MainViewController: KeyboardHandler, ReloadDelegate, AdjustHeightDelegate,
         if let editing = tableViewController?.tableView.isEditing {
             tableViewController?.tableView.setEditing(!editing, animated: true)
             navigationController?.setToolbarHidden(editing, animated: true)
-            
-            self.createContainerHeight.constant = editing ? 60 : 0
+
+            UIView.animate(withDuration: 1) {
+                self.bottomConstraint.constant = editing ? 5 : -65
+            }
             textContainerView.isHidden = !editing
         }
     }
@@ -40,6 +74,12 @@ class MainViewController: KeyboardHandler, ReloadDelegate, AdjustHeightDelegate,
         noteCreateViewController?.delegate = self
         tableViewController?.gesturesDelegate = self
         navigationController?.setToolbarHidden(true, animated: false)
+        
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Candies"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
     }
     
     // Perform Binding
